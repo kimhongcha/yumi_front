fdab23ce7f3ae6a4a8e0ced1314d94b27d9abaab
yumi <ckdballl@naver.com>
2021-01-17 06:59:47 +0900

    text commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import LoginComponent from "./components/LoginComponent";
+import SigninComponent from "./components/SigninComponent";
+import FeedComponent from "./components/FeedComponent";
+import MyPageComponent from "./components/MyPageComponent";
+import EditComponent from "./components/EditComponent";
+import ArchiveComponent from "./components/ArchiveComponent";
+import SavedComponent from "./components/SavedComponent";
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+          <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={LoginComponent}/>
+                <Route path="/signin" component={SigninComponent}/>
+                <Route path="/feed" component={FeedComponent}/>
+                <Route path="/mypage" component={MyPageComponent}/>
+                <Route path="/edit" component={EditComponent}/>
+                <Route path="/archive" component={ArchiveComponent}/>
+                <Route path="/saved" component={SavedComponent}/> 
+            </Switch>
+          </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
